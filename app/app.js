dd6c84ee2fca570816d98036783dbd339bb1e9f6
BilkisShakila <bilkisshakilaa@gmail.com>
2026-01-11 23:11:16 +0700

    edit port listen

--- app/app.js
+++ app/app.js
@@ -40,6 +40,6 @@ app.use("/", routes);
 // =====================
 // SERVER
 // =====================
-app.listen(PORT, "0, 0, 0, 0",() => {
+app.listen(PORT, "0.0.0.0",() => {
   console.log(`Server running on port ${PORT}`);
 });
