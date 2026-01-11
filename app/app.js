234a1d2604426122f3bad1c39aba26be8f291c21
BilkisShakila <bilkisshakilaa@gmail.com>
2026-01-11 22:36:40 +0700

    fix app listen

--- app/app.js
+++ app/app.js
@@ -40,6 +40,6 @@ app.use("/", routes);
 // =====================
 // SERVER
 // =====================
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+app.listen(PORT, "0, 0, 0, 0",() => {
+  console.log(`Server running on port ${PORT}`);
 });
