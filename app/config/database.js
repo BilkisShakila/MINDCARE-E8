a382cd72908d2f808c80457f7b33556f8ba5ae9f
BilkisShakila <bilkisshakilaa@gmail.com>
2026-01-12 00:40:24 +0700

    edit

--- app/config/database.js
+++ app/config/database.js
@@ -1,19 +1,15 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+// ✅ GUNAKAN CONNECTION POOL (WAJIB DI DOCKER)
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
-
-db.connect(err => {
-  if (err) {
-    console.error("MySQL error:", err);
-  } else {
-    console.log("MySQL connected");
-  }
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = db;
